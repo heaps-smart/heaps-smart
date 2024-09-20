09c25b76151df16dc741f1bee4eaef2960952f70
Derek Blank <derekpblank@gmail.com>
2024-09-20 12:03:21 +1000

    Invert static asset env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/heaps-smart/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/heaps-smart/' : '',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/heaps-smart',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/heaps-smart',
   trailingSlash: true,
   images: {
     unoptimized: true,
--- src/app/_components/hero-logo.tsx
+++ src/app/_components/hero-logo.tsx
@@ -1,14 +1,11 @@
 import { Logo } from "./logo";
 
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? "/heaps-smart" : '';
-
 export function HeroLogo() {
   return (
     <section
       className="flex items-center justify-center h-screen bg-cover bg-center"
       style={{
-        backgroundImage: `url('${basePath}/assets/img/rainforest.jpg')`,
+        backgroundImage: `url('/heaps-smart/assets/img/rainforest.jpg')`,
       }}
     >
       <div className="bg-black bg-opacity-50 p-8 aspect-square flex items-center justify-center">
