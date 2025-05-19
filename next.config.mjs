e23056daa1c9207141bc820cf73cae225f269405
Derek Blank <derekpblank@gmail.com>
2025-05-19 12:26:36 +1000

    Update casing

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
+// next.config.js
+import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '',
-  basePath: '',
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  webpack(config) {
+    config.plugins.push(new CaseSensitivePathsPlugin());
+    return config;
   },
 };
 
